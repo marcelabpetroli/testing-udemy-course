05c32a93c8b085f80b8a6ce1c63114c767e820d5
Marcela Petroli <marcelabpetroli@gmail.com>
2023-09-16 10:54:12 +0200

    test: add mocks folder and add test case using fs file

--- starting-project/src/util/io.test.js
+++ starting-project/src/util/io.test.js
@@ -21,7 +21,15 @@ it("should execute the writeFile method", () => {
 
   writeData(testData, testFilename);
 
-  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
   // expect(fs.writeFile).toBeCalled();
   expect(fs.writeFile).toBeCalledWith(testFilename, testData);
+});
+
+it("should return a promise that resolves to no value if called correctly", () => {
+  const testData = "test";
+  const testFilename = "test.txt";
+
+  writeData(testData, testFilename);
+
+  return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
 });
