107eda85420349407e5c97465b2456e298a7c93f
Marcela Petroli <marcelabpetroli@gmail.com>
2023-09-16 10:20:33 +0200

    refactor: use mock to avoid side-effects

--- starting-project/src/util/io.test.js
+++ starting-project/src/util/io.test.js
@@ -1,9 +1,17 @@
-import { it, expect } from "vitest";
-import writeData from "./io";
+import { it, expect, vi } from "vitest";
+import writeData from "./io.js";
+import { promises as fs } from "fs";
+
+vi.mock("fs");
+/* This kicks off vitest's auto-mocking algorithm.
+It'll find this module, and replace all the functions in there with empty spy functions */
 
 it("should execute the writeFile method", () => {
   const testData = "test";
   const testFilename = "test.txt";
 
-  return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  writeData(testData, testFilename);
+
+  /* return expect(writeData(testData, testFilename)).resolves.toBeUndefined(); */
+  expect(fs.writeFile).toBeCalled();
 });
